c3c0b147ecc43c9708b5f487ba167fcf77e3a173
Jéssica Nascimento <jessica.cfsb@gmail.com>
2022-06-24 18:19:01 -0300

    aula 02 - criando a classe pessoa

---
+++ projetojava1/src/curso/java/jessica/Pessoa.java
@@ -0,0 +1,28 @@
+package curso.java.jessica;
+
+public class Pessoa {
+		private String nome;
+		private String sobreNome;
+		private int idade;
+		
+		public int getIdade() {
+			return idade;
+		}
+		public void setIdade(int idade) {
+			this.idade = idade;
+		}
+		public String getNome() {
+			return nome;
+		}
+		public void setNome(String nome) {
+			this.nome = nome;
+		}
+		public String getSobreNome() {
+			return sobreNome;
+		}
+		public void setSobreNome(String sobreNome) {
+			this.sobreNome = sobreNome;
+		}
+		
+		
+}
