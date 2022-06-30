ce115eeee93fd9e3ac3dd49fbd6ab5923eabeeaa
Jéssica Nascimento <jessica.cfsb@gmail.com>
2022-06-30 14:55:48 -0300

    Trabalhando input de dados

--- projetojava1/src/cursojava/executavel/PrimeiraClasseJava.java
+++ projetojava1/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -1,33 +1,52 @@
 package cursojava.executavel;
 
+import javax.swing.JOptionPane;
+
 import cursojava.classes.Aluno;
 
 public class PrimeiraClasseJava {
 	public static void main(String[] args) {
+		//entrada de dados
+		String nome = JOptionPane.showInputDialog("Qual o nome do aluno?");
+		String idade = JOptionPane.showInputDialog("Qual a idade do aluno?");
+		String dataNascimento = JOptionPane.showInputDialog("Qual a data de nascimento do aluno?");
+		String rg = JOptionPane.showInputDialog("Qual o rg do aluno?");
+		String cpf = JOptionPane.showInputDialog("Qual o cpf do aluno?");
+		String mae = JOptionPane.showInputDialog("Qual o nome da mãe do aluno?");
+		String pai = JOptionPane.showInputDialog("Qual o nome do pai do aluno?");
+		String dataMatricula = JOptionPane.showInputDialog("Qual data da matrícula do aluno?");
+		String serie = JOptionPane.showInputDialog("Qual a série do aluno?");
+		String escola = JOptionPane.showInputDialog("Qual a escola do aluno?");
+		String nota1 = JOptionPane.showInputDialog("Primeira nota: ");
+		String nota2 = JOptionPane.showInputDialog("Segunda nota: ");
+		String nota3 = JOptionPane.showInputDialog("Terceira nota: ");
+		String nota4 = JOptionPane.showInputDialog("Quarta nota: ");
+		
 		//vc chama a classe que deseja iniciar (criar o obj)
 		//a partir deste momento o aluno se torna um obj
 		Aluno aluno1 = new Aluno(); //aqui será o joao
-		aluno1.setNome("Jõao da Silva");
-		aluno1.setIdade(50);
-		aluno1.setDataNascimento("18/10/1987");
-		aluno1.setRegistroGeral("445454-2");
-		aluno1.setNumeroCpf("123.123.123-12");
-		aluno1.setNomeMae("Shirlei");
-		aluno1.setNomePai("Antonio");
-		aluno1.setDataMatricula("10/01/2019");
-		aluno1.setSerieMatriculado("5");
-		aluno1.setNomeEscola("Escola Jdev");
-		aluno1.setNota1(90);
-		aluno1.setNota2(80.8);
-		aluno1.setNota3(70.9);
-		aluno1.setNota4(90.7);
+		aluno1.setNome(nome);
+		aluno1.setIdade(Integer.valueOf(idade));
+		aluno1.setDataNascimento(dataNascimento);
+		aluno1.setRegistroGeral(rg);
+		aluno1.setNumeroCpf(cpf);
+		aluno1.setNomeMae(mae);
+		aluno1.setNomePai(pai);
+		aluno1.setDataMatricula(dataMatricula);
+		aluno1.setSerieMatriculado(serie);
+		aluno1.setNomeEscola(escola);
+		aluno1.setNota1(Double.parseDouble(nota1));
+		aluno1.setNota2(Double.parseDouble(nota2));
+		aluno1.setNota3(Double.parseDouble(nota3));
+		aluno1.setNota4(Double.parseDouble(nota4));
 		
 		System.out.println("Nome é: " + aluno1.getNome());
 		System.out.println("Idade é: " + aluno1.getIdade());
+		System.out.println("Nome da Mãe: " + aluno1.getIdade());
 		System.out.println("Nascimento: " + aluno1.getDataNascimento());
 		System.out.println("Média da nota é = " + aluno1.getMediaNota());
 		System.out.println("Resultado = " + aluno1.getAlunoAprovado());
-		System.out.println("Resultado = " + (aluno1.getAlunoAprovado2() ? "Aprovado" : "Reprovado")); //com operador ternario
+		System.out.println("Resultado 2 = " + (aluno1.getAlunoAprovado2() ? "Aprovado" : "Reprovado")); //com operador ternario
 		
 		//--------------------------------------------------------------------
 		
