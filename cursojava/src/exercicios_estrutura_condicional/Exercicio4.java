57c138e1d5c0859559d95674d814b6d8afdbc971
Jéssica Nascimento <jessica.cfsb@gmail.com>
2022-07-18 16:18:10 -0300

    Estrutura condicional

--- cursojava/src/exercicios_estrutura_condicional/Exercicio4.java
+++ cursojava/src/exercicios_estrutura_condicional/Exercicio4.java
@@ -1,10 +1,25 @@
 package exercicios_estrutura_condicional;
 
+import java.util.Scanner;
+
 public class Exercicio4 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner scanner = new Scanner(System.in);
+		
+		int inicio, fim, duracao;
+		
+		inicio = scanner.nextInt();
+		fim = scanner.nextInt();
+		
+		if (inicio < fim) {
+			duracao = fim - inicio;
+		}
+		else {
+			duracao = 24 - inicio + fim;
+		}
+		
+		System.out.println("O JOGO DUROU " + duracao + " HORA(S)");
 	}
 
 }
