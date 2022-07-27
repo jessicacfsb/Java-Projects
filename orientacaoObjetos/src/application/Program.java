fe19f1d407cbf1aaa382528d5ac62a7afc079e39
Jéssica Nascimento <jessica.cfsb@gmail.com>
2022-07-27 18:25:25 -0300

    Versão com POO (atividade triangulo)

--- orientacaoObjetos/src/application/Program.java
+++ orientacaoObjetos/src/application/Program.java
@@ -3,28 +3,29 @@ package application;
 import java.util.Locale;
 import java.util.Scanner;
 
-//programa SEM Programação orientada à objetos
+import entities.Triangle;
+
+//programa COM Programação orientada à objetos
 
 public class Program {
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		Scanner scanner = new Scanner(System.in);
-		double xA, xB, xC, yA, yB, yC;
+
+		Triangle x = new Triangle();
+		Triangle y = new Triangle();
 		
 		System.out.println("Entre com as medidas do triangulo x: ");
-		xA = scanner.nextDouble();
-		xB = scanner.nextDouble();
-		xC = scanner.nextDouble();
+		x.a = scanner.nextDouble();
+		x.b = scanner.nextDouble();
+		x.c = scanner.nextDouble();
 		System.out.println("Entre com as medidas do triangulo x: ");
-		yA = scanner.nextDouble();
-		yB = scanner.nextDouble();
-		yC = scanner.nextDouble();
-		
-		double p = (xA + xB + xC) / 2.0;
-		double areaX = Math.sqrt(p* (p - xA) * (p - xB) * (p - xC));
-		
-		p = (yA + yB + yC) / 2.0;
-		double areaY = Math.sqrt(p* (p - yA) * (p - yB) * (p - yC));
+		y.a = scanner.nextDouble();
+		y.b = scanner.nextDouble();
+		y.c = scanner.nextDouble();
+	
+		double areaX = x.area();
+		double areaY = y.area();
 		
 		System.out.printf("Triangle X area: %.4f%n", areaX);
 		System.out.printf("Triangle Y area: %.4f%n", areaY);
